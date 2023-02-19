d75e3af86e2dbe9205bf3d072b188a5548b2b19b
Stetsiv Andrii <steciv14@gmail.com>
2023-02-19 17:42:04 +0100

    Added component DidUpdate i DidMount

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,38 +2,37 @@ class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=dcd4a4957000efad9ab30bb1c9bf34ac';
 
-  getResourse = async (url) => {
+  getResource = async (url) => {
     let res = await fetch(url);
 
-    if (!res) {
-      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
     return await res.json();
-  };
-  getAllCharacters = async () => {
-    const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-    return res.data.results.map(this._tranformCharachter);
-  };
-  getCharacters =  async (id) => {
-    const res = await this.getResourse(
-      `${this._apiBase}characters/${id}?${this._apiKey}`
-    );
-    return this._tranformCharachter(res.data.results[0])
-  };
+}
 
-  _tranformCharachter = (char) => {
+getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
+}
+
+getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
+}
+
+_transformCharacter = (char) => {
     return {
-      name: char.name,
-      description: char.description,
-      thumbnail:
-        char.thumbnail.path +
-        '.' +
-        char.thumbnail.extension,
-      homepage: char.urls[0].url,
-      wiki: char.urls[1].url
+        id: char.id,
+        name: char.name,
+        description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+        thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+        homepage: char.urls[0].url,
+        wiki: char.urls[1].url,
+        comics: char.comics.items
     }
-  }
+}
 }
 
 export default MarvelService;
